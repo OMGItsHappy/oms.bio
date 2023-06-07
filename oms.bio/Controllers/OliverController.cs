df5729a1f79e04dbae05718ab5306a928f178db5
Oliver Schwab <61769826+OMGItsHappy@users.noreply.github.com>
2023-06-07 14:37:14 -0600

    With Image

--- oms.bio/Controllers/OliverController.cs
+++ oms.bio/Controllers/OliverController.cs
@@ -6,16 +6,18 @@ using System.Net.Mail;
 namespace oms.bio.Controllers
 {
     public class OliverController : Controller
-    {
-        [DisplayName("My Index")]
-        private IActionResult Index()
-        {
-            return View();
-        }
+    { //https://coolors.co/c2c094-a9a587-390040-730071-dc9596
+        //https://coolors.co/ffa400-009ffd-2a2a72-232528-eaf6ff
         [DisplayName("About Me")]
         public IActionResult Me()
         {
             MyFunctions.GetMethods("OliverController");
+            return View();
+        }
+
+        [DisplayName("My Resume")]
+        public IActionResult Resume() 
+        {
             return View();
         }
     }
